865374c16e73ef33016f52ac0c720a52366541bc
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-06-26 11:17:44 -0400

    integrated unsplash api

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Unsplash API
+const count = 10;
+const apiKey = "MbwKGKa6GCTmg1OBDWSRhWQYqwtRebtxgPA_l7DvZLE";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}
+`;
 
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {}
+}
+
+//On load
+getPhotos()
